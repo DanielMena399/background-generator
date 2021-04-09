5709165164101e82d1589ceb8f7f51a98021e956
DanielMena399 <46728776+DanielMena399@users.noreply.github.com>
2021-04-09 10:09:18 -0400

    My first commit

---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'leannegraham@gmailemail.com',
+    },
+    {
+        id: 2,
+        name: 'Dat boi',
+        username: 'elpepe',
+        email: 'datboi@gmailemail.com',
+    },
+    {
+        id: 3,
+        name: 'suki tuki',
+        username: 'suki',
+        email: 'sukituki@gmailemail.com',
+    },
+    {
+        id: 4,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'leannegraham@gmailemail.com',
+    },
+    {
+        id: 5,
+        name: 'Dat boi',
+        username: 'elpepe',
+        email: 'datboi@gmailemail.com',
+    },
+    {
+        id: 6,
+        name: 'suki tuki',
+        username: 'suki',
+        email: 'sukituki@gmailemail.com',
+    },
+];
